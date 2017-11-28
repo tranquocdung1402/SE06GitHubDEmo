3524e2c10b9b84cfd6d76609eb389e22fd32faa5
PC <PC@PC-PC>
2017-11-28 08:36:45 +0700

    First Verson 2

--- src/java/buciness/Student.java
+++ src/java/buciness/Student.java
@@ -12,6 +12,22 @@ public class Student {
         this.studentID = studentID;
         this.studentName = studentName;
     }
+
+    public int getStudentID() {
+        return studentID;
+    }
+
+    public void setStudentID(int studentID) {
+        this.studentID = studentID;
+    }
+
+    public String getStudentName() {
+        return studentName;
+    }
+
+    public void setStudentName(String studentName) {
+        this.studentName = studentName;
+    }
     
     
 }
