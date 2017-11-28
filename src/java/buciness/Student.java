51d6f64ae15eeedacec533112c5610a02411bdc4
PC <PC@PC-PC>
2017-11-28 08:29:55 +0700

    First Verson

---
+++ src/java/buciness/Student.java
@@ -0,0 +1,17 @@
+
+package buciness;
+
+
+public class Student {
+    private int studentID;
+    private String studentName;
+    
+    
+    //This is a contructor
+    public Student(int studentID, String studentName) {
+        this.studentID = studentID;
+        this.studentName = studentName;
+    }
+    
+    
+}
